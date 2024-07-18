3215b8a852ee8a4575c200c1999902f37ba9d268
Ahmet Ozsari <73311698+ozsari@users.noreply.github.com>
2024-07-18 14:02:28 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.set_page_config(page_title="Ahmet Ozsari - CV") # , layout="wide"
 
 # Header
 st.title("Ahmet Ozsari")
-st.subheader("Experienced Data Engineer")
+st.subheader("Data Engineer")
 st.write("📍 Istanbul, TURKEY")
 st.write("📧 ozsariahmet@yahoo.com | 🔗 [LinkedIn](https://linkedin.com/in/ahmet-ozsari-6615b756)")
 
