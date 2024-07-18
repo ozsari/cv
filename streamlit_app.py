ba99c0df278684cafe085cbe1996fd024d7514f4
Ahmet Ozsari <73311698+ozsari@users.noreply.github.com>
2024-07-18 13:20:45 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("![curriculum-vitae](https://github.com/user-attachments/assets/178ad900-0d36-467d-88cb-30f6d353cf94) My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
